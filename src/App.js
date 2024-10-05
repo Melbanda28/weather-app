608fc843cad49964187429092baa054cd927a8eb
Melbanda28 <122828898+Melbanda28@users.noreply.github.com>
2024-10-05 16:25:29 +0000

    CREATED ALL JS FOLDERS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import Weather from './Weather';
-import './App.css';
- 
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Weather />
-      </header>
+      <div className="container">
+        <Weather defaultCity="Cape Town" />
+      </div>
     </div>
   );
 }
-
-export default App;
